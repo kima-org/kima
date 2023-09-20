e9f5cef7269f2f117deee68c8bd76bd2840dc90b
João Faria <joao.faria@astro.up.pt>
2023-09-20 15:12:47 +0200

    new tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -15,4 +15,20 @@ def test_api():
     m.trend = True
     m.degree = 2
     # print(m.trend)
-    print(help(kima.run))
+    # print(help(kima.run))
+
+def test_RVData():
+    D = kima.RVData('tests/simulated1.txt')
+    D = kima.RVData(['tests/simulated1.txt', 'tests/simulated2.txt'])
+
+def test_GPmodel():
+    D = kima.RVData('tests/simulated1.txt')
+    m = kima.GPmodel(True, 0, D)
+    # kima.run(m)
+
+
+def test_distributions():
+    from kima import distributions
+    from kima.distributions import Gaussian, Uniform
+    u = Uniform()
+    print(u)
