4dd4f25c43a54f3c7b0705e543c41058d61deb7e
João Faria <joao.faria@astro.up.pt>
2023-09-22 16:22:52 +0200

    missing h file

---
+++ src/kima/nb_shared.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#if defined(_WIN32)
+#  define KIMA_EXPORT __declspec(dllexport)
+#  define KIMA_IMPORT __declspec(dllimport)
+#else
+#  define KIMA_EXPORT __attribute__ ((visibility("default")))
+#  define KIMA_IMPORT __attribute__ ((visibility("default")))
+#endif
+
+#if defined(KIMA_BUILD)
+#  define KIMA_API KIMA_EXPORT
+#else
+#  define KIMA_API KIMA_IMPORT
+#endif
