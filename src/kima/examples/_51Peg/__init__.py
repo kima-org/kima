1492040ed313530ab3b2d58c7d79b8126c8730b2
João Faria <joao.faria@unige.ch>
2023-10-16 16:32:36 +0200

    str -> list

--- src/kima/examples/_51Peg/__init__.py
+++ src/kima/examples/_51Peg/__init__.py
@@ -18,10 +18,7 @@ def _51Peg(run=False, **kwargs):
         **kwargs: keyword arguments passed directly to `kima.run`
     """
     # load the right data file
-    print(os.path.join(here, '51Peg.rv'))
-    with open(os.path.join(here, '51Peg.rv')) as f:
-        print(f.read())
-    data = RVData(os.path.join(here, '51Peg.rv'))
+    data = RVData([os.path.join(here, '51Peg.rv')])
     print(data)
     # create the model
     model = RVmodel(fix=False, npmax=1, data=data)
@@ -39,5 +36,5 @@ def _51Peg(run=False, **kwargs):
     return model
 
 if __name__ == '__main__':
-    model = _51Peg(run=True)
+    model = _51Peg(run=True, steps=10)
     res = kima.load_results()
