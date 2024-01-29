fefe94b84e49d31151c2f86ba86be8569847095b
João Faria <joao.faria@unige.ch>
2024-01-29 17:00:39 +0100

    change options for multi_instrument example; add load results

--- src/kima/examples/multi_instruments/__init__.py
+++ src/kima/examples/multi_instruments/__init__.py
@@ -19,35 +19,38 @@ HD106252_combined = RVData([
     os.path.join(here, 'HD106252_Lick.txt'),
 ])
 
-def multi_instruments(run=False, **kwargs):
+def multi_instruments(run=False, load=False, **kwargs):
     """
     Create (and optionally run) an RV model for analysis of HD106252 data from
     multiple instruments, namely ELODIE, HET, HJS, and Lick. 
 
     Args:
         run (bool): whether to run the model
+        load (bool): load results after running
         **kwargs: keyword arguments passed directly to `kima.run`
     """
     data = HD106252_combined
 
     # create the model
-    model = RVmodel(fix=True, npmax=0, data=data)
+    model = RVmodel(fix=False, npmax=1, data=data)
 
-    model.Cprior = get_gaussian_prior_vsys(data)
-    model.individual_offset_prior = get_gaussian_priors_individual_offsets(data)
+    #model.Cprior = get_gaussian_prior_vsys(data)
+    #model.individual_offset_prior = get_gaussian_priors_individual_offsets(data)
 
-    kwargs.setdefault('steps', 20_000)
+    kwargs.setdefault('steps', 50_000)
     kwargs.setdefault('num_threads', 4)
-    kwargs.setdefault('num_particles', 2)
-    kwargs.setdefault('new_level_interval', 2000)
-    kwargs.setdefault('save_interval', 200)
+    kwargs.setdefault('num_particles', 4)
+    kwargs.setdefault('new_level_interval', 5000)
+    kwargs.setdefault('save_interval', 500)
 
     if run:
         with chdir(here):
             kima.run(model, **kwargs)
+            if load:
+                res = kima.load_results()
+                return model, res
 
     return model
 
 if __name__ == '__main__':
-    model = multi_instruments(run=True)
-    res = kima.load_results()
+    model, res = multi_instruments(run=True, load=True)
