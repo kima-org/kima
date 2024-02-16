df9669ff38eddb1222bee1c0ba41aea5c599c552
João Faria <joao.faria@unige.ch>
2024-02-16 14:47:49 +0100

    docs for keplerian

--- src/kima/kepler.pyi
+++ src/kima/kepler.pyi
@@ -19,6 +19,29 @@ def contour_solver(M: float, ecc: float) -> float:
     ...
 
 def keplerian(t: list[float], P: float, K: float, ecc: float, w: float, M0: float, M0_epoch: float) -> list[float]:
+    """
+    Calculate the Keplerian curve at times `t`
+    
+    Args:
+    t (array):
+    Times at which to calculate the Keplerian function
+    P (float):
+    Orbital period [days]
+    K (float):
+    Semi-amplitude
+    ecc (float):
+    Orbital eccentricity
+    w (float):
+    Argument of periastron [rad]
+    M0 (float):
+    Mean anomaly at the epoch [rad]
+    M0_epoch (float):
+    Reference epoch for the mean anomaly (M=0 at this time) [days]
+    
+    Returns:
+    v (array):
+    Keplerian function evaluated at input times `t`
+    """
     ...
 
 def keplerian2(t: list[float], P: float, K: float, ecc: float, w: float, M0: float, M0_epoch: float) -> list[float]:
