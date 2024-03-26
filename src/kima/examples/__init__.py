577852f055a6c1dd1bf3d2d02b7fb950555b0616
João Faria <joao.faria@unige.ch>
2024-03-26 15:56:34 +0100

    import examples

--- src/kima/examples/__init__.py
+++ src/kima/examples/__init__.py
@@ -1,2 +1,6 @@
 from ._51Peg import _51Peg
 from .multi_instruments import multi_instruments
+from .BL2009 import BL2009
+from .CoRoT7 import CoRoT7
+
+__all__ = ['_51Peg', 'multi_instruments', 'BL2009', 'CoRoT7']
