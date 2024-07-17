22fa9103d2d4abb99b3290b6f0183c7005ecac2c
João Faria <joao.faria@unige.ch>
2024-07-17 16:59:53 +0200

    create a test for the phase_plot function

--- tests/common.py
+++ tests/common.py
@@ -1,3 +1,7 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from kima import RVData, keplerian
+
 import pytest
 
 @pytest.fixture
@@ -5,3 +9,75 @@ def cleanup_after_running():
     from kima.pykima.cli import cli_clean
     yield
     cli_clean(check=False, output=True)
+
+
+def create_data(plot=False, include=(1, 2, 4, 6), seed=24):
+
+    # calculate time of transit
+    def _get_Tc(P, e, w, Tp) -> float: 
+        f = np.pi/2 - w
+        E = 2 * np.arctan(np.tan(f/2) * np.sqrt((1-e)/(1+e)))
+        Tc = Tp + P/(2*np.pi) * (E - e*np.sin(E))
+        return Tc
+
+    include = np.array(include) - 1
+    np.random.seed(seed)
+
+    t = np.sort(np.random.uniform(0, 100, 98))
+    tt = np.linspace(t.min(), t.max(), 2000)
+    err = np.random.uniform(0.1, 0.3, t.size)
+    M0_epoch = t[0]
+
+    v = np.zeros_like(t)
+
+    # transiting planets
+    par1 = dict(P=29, K=2, e=0.6, w=0.1, Tp=10)
+    par1['M0'] = 2 * np.pi * (M0_epoch - par1['Tp']) / par1['P']
+    par1['Tc'] = _get_Tc(par1['P'], par1['e'], par1['w'], par1['Tp'])
+
+    par2 = dict(P=19, K=1.8, e=0.3, w=0.9, Tp=15)
+    par2['M0'] = 2 * np.pi * (M0_epoch - par2['Tp']) / par2['P']
+    par2['Tc'] = _get_Tc(par2['P'], par2['e'], par2['w'], par2['Tp'])
+
+    par3 = dict(P=11, K=0.8, e=0.1, w=2.1, Tp=44)
+    par3['M0'] = 2 * np.pi * (M0_epoch - par3['Tp']) / par3['P']
+    par3['Tc'] = _get_Tc(par3['P'], par3['e'], par3['w'], par3['Tp'])
+
+    # known objects
+    par4 = dict(P=43, K=1.3, e=0.1, w=0.1, M0=0.0)
+    par5 = dict(P=83, K=1.0, e=0.0, w=0.3, M0=2.0)
+
+    # *normal* planet
+    par6 = dict(P=2.1, K=1.0, e=0.0, w=0.3, M0=0.0)
+
+    pars = (par1, par2, par3, par4, par5, par6)
+
+    for par in np.array(pars)[include]:
+        v += keplerian(t, par['P'], par['K'], par['e'], par['w'], par['M0'], M0_epoch)
+
+    # add some small Gaussian noise
+    v += np.random.normal(loc=0.0, scale=0.05, size=t.size)
+
+    # add a linear trend
+    trend = [0.2, -20]
+    tmiddle = t.min() + np.ptp(t) / 2
+    v += np.polyval(trend, t - tmiddle)
+
+    if plot:
+        fig, ax = plt.subplots(constrained_layout=True)
+        ax.errorbar(t, v, yerr=err, fmt='o', label='data')
+
+        vv = np.zeros_like(tt)
+        for i, par in enumerate(np.array(pars)[include]):
+            _v = keplerian(tt, par['P'], par['K'], par['e'], par['w'], par['M0'], M0_epoch)
+            vv += _v
+            ax.plot(tt, _v + 30, zorder=-1, alpha=0.3, label=f'planet {include[i]+1}')
+        ax.plot(tt, vv + 20, label='planets', zorder=-1)
+        ax.plot(tt, vv + np.polyval(trend, tt - tmiddle), label='full', color='k', zorder=-1)
+        ax.legend()
+
+    return (
+        (t, v, err),
+        (par1, par2, par3, par4, par5, par6, trend)
+    )
+
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import pytest
 from common import cleanup_after_running
 
@@ -94,7 +95,7 @@ def test_RVFWHMmodel():
                          kima.RVData('tests/simulated2.txt', indicators=['i', 'j']))
 
 
-def test_run():
+def test_run(cleanup_after_running):
     m = kima.RVmodel(True, 0, kima.RVData('tests/simulated1.txt'))
     kima.run(m, steps=1)
     m = kima.GPmodel(True, 0, kima.RVData('tests/simulated1.txt'))
@@ -103,3 +104,109 @@ def test_run():
     kima.run(m, steps=1)
 
 
+
+@pytest.mark.parametrize('include, KO, nKO, TR, nTR, nP',
+                         [
+                            ((1, 4), True, 1, True, 1, 0),
+                            ((1, 2, 3, 4, 5, 6), True, 2, True, 3, 1),
+                            ((1, 2, 4, 6), True, 1, True, 2, 1),
+                         ])
+@pytest.mark.mpl_image_compare
+def test_phase_plot(include, KO, nKO, TR, nTR, nP):
+    from kima.pykima.display import phase_plot
+    from kima.pykima.results import data_holder, KimaResults
+    from common import create_data
+
+    class FakeResult:
+        max_components = 2
+        model = 'RVmodel'
+        has_gp = studentt = False
+        full_model = KimaResults.full_model
+        eval_model = KimaResults.eval_model
+        planet_model = KimaResults.planet_model
+        stochastic_model = KimaResults.stochastic_model
+        residuals = KimaResults.residuals
+        n_instruments = 1
+        instruments = 'data'
+        multi = arbitrary_units = save_plots = False
+        return_figs = True
+        @property
+        def _time_overlaps(self):
+            raise ValueError
+
+    data, pars = create_data(plot=False, include=include)
+    par1, par2, par3, par4, par5, par6, trend = pars
+    d = data_holder()
+    d.t, d.y, d.e = data
+    d.N = d.t.size
+    d.obs = np.ones_like(d.t, dtype=int)
+    
+    res = FakeResult()
+    res.trend, res.degree = True, 1
+    res.KO, res.nKO = KO, nKO
+    res.TR, res.nTR = TR, nTR
+    res.total_parameters = 2 + res.nKO*5 + res.nTR*5 + 1 + 2*5 + 1
+    res.data = d
+    res.M0_epoch = d.t[0]
+    res.indices = {
+        'trend': slice(1, 2, None),
+        'KOpars': slice(2, 2 + res.nKO*5, None),
+        'TRpars': slice(2 + res.nKO*5, 2 + res.nKO*5 + res.nTR*5, None),
+        'np': 2+res.nKO*5+res.nTR*5+2,
+        'planets': slice(2+res.nKO*5+res.nTR*5+2+1, 2+res.nKO*5+res.nTR*5+2+1 + 10, None),
+        'planets.P': slice(2+res.nKO*5+res.nTR*5+2+1 + 0, 2+res.nKO*5+res.nTR*5+2+1 + 2, None),
+        'planets.K': slice(2+res.nKO*5+res.nTR*5+2+1 + 2, 2+res.nKO*5+res.nTR*5+2+1 + 4, None),
+        'planets.φ': slice(2+res.nKO*5+res.nTR*5+2+1 + 4, 2+res.nKO*5+res.nTR*5+2+1 + 6, None),
+        'planets.e': slice(2+res.nKO*5+res.nTR*5+2+1 + 6, 2+res.nKO*5+res.nTR*5+2+1 + 8, None),
+        'planets.w': slice(2+res.nKO*5+res.nTR*5+2+1 + 8, 2+res.nKO*5+res.nTR*5+2+1 + 10, None),
+        'vsys': -1
+    }
+
+                # jitter, slope
+    p = np.array([0.0,    trend[0]])
+
+    if res.nKO == 1:
+        p = np.r_[p, [par4['P'], par4['K'], par4['M0'], par4['e'], par4['w']]]
+    elif res.nKO == 2:
+        # KO:         P1, P2, K1,  K2,  φ1,  φ2,  e1,  e2,  w1,  w2
+        p = np.r_[p, [par4['P'], par5['P'], 
+                      par4['K'], par5['K'], 
+                      par4['M0'], par5['M0'], 
+                      par4['e'], par5['e'], 
+                      par4['w'], par5['w']]
+        ]
+
+    if res.nTR == 1:
+        p = np.r_[p, [par1['P'], par1['K'], par1['Tc'], par1['e'], par1['w']]]
+    elif res.nTR == 2:
+        # TR:         P1, P2, K1, K2,  Tc1,           Tc2,           e1,  e2,  w1,  w2
+        p = np.r_[p, [par1['P'], par2['P'],
+                      par1['K'], par2['K'],
+                      par1['Tc'], par2['Tc'],
+                      par1['e'], par2['e'],
+                      par1['w'], par2['w']]
+        ]
+    elif res.nTR == 3:
+        p = np.r_[p, [par1['P'], par2['P'], par3['P'],
+                      par1['K'], par2['K'], par3['K'],
+                      par1['Tc'], par2['Tc'], par3['Tc'],
+                      par1['e'], par2['e'], par3['e'],
+                      par1['w'], par2['w'], par3['w']]
+        ]
+    
+                # ndim, maxnp, np
+    p = np.r_[p, [5,    2,     nP]]
+
+    if nP == 0:
+        #              P1,  P2,  K1,  K2,  φ1,  φ2,  e1,  e2,  w1,  w2
+        p = np.r_[p, [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]]
+    elif nP == 1:
+        #            P1,         P2,  K1,        K2,  φ1,         φ2,  e1,        e2,  w1,        w2
+        p = np.r_[p, [par6['P'], 0.0, par6['K'], 0.0, par6['M0'], 0.0, par6['e'], 0.0, par6['w'], 0.0]]        
+
+    #             staleness, vsys
+    p = np.r_[p, [0,         trend[1]]]
+
+    fig = phase_plot(res, p)
+    plt.show()
+    return fig
