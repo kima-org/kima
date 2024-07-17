719923cb07a3866b9d4ceccb12c6b7b1d318c2a6
João Faria <joao.faria@unige.ch>
2024-07-17 17:00:35 +0200

    docs for hpd_grid

--- src/kima/pykima/analysis.py
+++ src/kima/pykima/analysis.py
@@ -1275,23 +1275,22 @@ def _columns_with_dynamic_range(results):
 
 def hpd_grid(sample, alpha=0.05, bw_method=None, roundto=2):
     """Calculate highest posterior density (HPD) of array for given alpha. 
-    The HPD is the minimum width Bayesian credible interval (BCI). 
-    The function works for multimodal distributions, returning more than one mode
+    The HPD is the minimum width Bayesian credible interval (BCI). The function
+    works for multimodal distributions, returning more than one mode.
 
-    Parameters
-    ----------
-    
-    sample : Numpy array or python list
-        An array containing MCMC samples
-    alpha : float
-        Desired probability of type I error (defaults to 0.05)
-    roundto: integer
-        Number of digits after the decimal point for the results
+    Warning:
+        This function is experimental.
 
-    Returns
-    ----------
-    hpd: array with the lower 
-          
+    Args:
+        sample (array):
+            An array containing MCMC samples
+        alpha (float, optional):
+            Desired probability of type I error (defaults to 0.05)
+        bw_method (float, optional):
+            The method used to estimate the KDE bandwidth. See
+            `scipy.stats.gaussian_kde`
+        roundto (integer):
+            Number of digits after the decimal point for the results
     """
     import scipy.stats.kde as kde
     sample = np.asarray(sample)
