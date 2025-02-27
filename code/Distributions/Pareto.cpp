0e2b9d56288b82ee4ea983a3a7142039cacfbecc
João Faria <joao.faria@unige.ch>
2025-02-27 18:29:41 +0100

    Squashed 'src/vendor/DNest4/' changes from 2d075002..0af4a93b
    0af4a93b fix issues with Pareto distribution
    fb9bc1b8 mark setpars as override
    
    git-subtree-dir: src/vendor/DNest4
    git-subtree-split: 0af4a93bc4a2454bab4108aa5f64e43711b8b641

--- code/Distributions/Pareto.cpp
+++ code/Distributions/Pareto.cpp
@@ -16,23 +16,23 @@ Pareto::Pareto(double _min, double _alpha)
 
 double Pareto::cdf(double x) const
 {
-    if(x <= min)
+    if(x < min)
         return 0.0;
 
-    return 1.0 - pow(x/min, alpha);
+    return 1.0 - pow(min/x, alpha);
 }
 
 double Pareto::cdf_inverse(double x) const
 {
     if(x < 0.0 || x > 1.0)
         throw std::domain_error("Input to cdf_inverse must be in [0, 1].");
-    return min*pow(1.0 - x, 1.0/alpha);
+    return min*pow(1.0 - x, -1.0/alpha);
 }
 
 double Pareto::log_pdf(double x) const
 {
     if(x <= min)
-        return -1E300;
+        return -std::numeric_limits<double>::infinity();
     return log(alpha) + alpha*log(min) - (alpha + 1.0)*log(x);
 }
 
