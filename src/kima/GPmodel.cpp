460b6feb115cd458e44d59dc094dda121d9b8644
João Faria <joao.faria@unige.ch>
2025-03-05 10:36:01 +0100

    GPmodel: perturb planet parameters only if there are planets

--- src/kima/GPmodel.cpp
+++ src/kima/GPmodel.cpp
@@ -570,8 +570,8 @@ double GPmodel::perturb(RNG& rng)
     double logH = 0.;
     double tmid = data.get_t_middle();
 
-
-    if(rng.rand() <= 0.5) // perturb planet parameters
+    int maxpl = planets.get_max_num_components();
+    if(maxpl > 0 && rng.rand() <= 0.5) // perturb planet parameters
     {
         logH += planets.perturb(rng);
         planets.consolidate_diff();
