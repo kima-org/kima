8ba3f5ce0ec4cd27329f0f9d003c4bf8df687b55
ThomasBaycroft <thomasbaycroft@hotmail.co.uk>
2025-05-14 14:05:35 +0100

    Change GAIA inclination prior

--- src/kima/ConditionalPrior.cpp
+++ src/kima/ConditionalPrior.cpp
@@ -265,7 +265,7 @@ GAIAConditionalPrior::GAIAConditionalPrior():thiele_innes(false)
         if (!omegaprior)
             omegaprior = make_shared<Uniform>(0, 2*M_PI);
         if (!cosiprior)
-            cosiprior = make_shared<Uniform>(-1, 1);
+            cosiprior = make_shared<Uniform>(0, 1);
         if (!Omegaprior)
             Omegaprior = make_shared<Uniform>(0, M_PI);
     }
@@ -403,7 +403,7 @@ RVGAIAConditionalPrior::RVGAIAConditionalPrior()
     if (!omegaprior)
         omegaprior = make_shared<Uniform>(0, 2*M_PI);
     if (!cosiprior)
-        cosiprior = make_shared<Uniform>(-1, 1);
+        cosiprior = make_shared<Uniform>(0, 1);
     if (!Omegaprior)
         Omegaprior = make_shared<Uniform>(0, 2*M_PI);
     
