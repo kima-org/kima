de105dea04f453712d00578394fdddfd73fe2f71
João Faria <joao.faria@unige.ch>
2025-07-04 19:10:15 +0200

    run 51Peg as benchmark

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,15 +1,8 @@
-import pytest 
-import time
+from kima.examples import _51Peg
 
-def something(duration=0.000001):
+def run_51Peg():
     """ Function that needs some serious benchmarking """
-    time.sleep(duration)
-    # You may return anything you want, like the result of a computation
-    return 123
+    _ = _51Peg(run=True, steps=100)
 
 def test_my_stuff(benchmark):
-    # benchmark something
-    result = benchmark(something)
-
-    # Extra code, to verify that the run completed correctly.
-    assert result == 123
+    _ = benchmark(run_51Peg)
