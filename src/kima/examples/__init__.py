71548b7fa7376e08515d140d697a65de6d342d69
ThomasBaycroft <thombaycroft@gmail.com>
2025-12-01 11:23:45 +0800

    Start adding Gaiamodel to examples

--- src/kima/examples/__init__.py
+++ src/kima/examples/__init__.py
@@ -3,5 +3,6 @@ from .multi_instruments import multi_instruments
 from .BL2009 import BL2009
 from .CoRoT7 import CoRoT7
 from .TIC172900988 import TIC172900988
+from .GaiaBH3.GAIA_free import GAIA_free
 
-__all__ = ['_51Peg', 'multi_instruments', 'BL2009', 'CoRoT7', 'TIC172900988']
+__all__ = ['_51Peg', 'multi_instruments', 'BL2009', 'CoRoT7', 'TIC172900988','GAIA_free']
