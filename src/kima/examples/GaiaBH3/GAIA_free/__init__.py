856d71d9379ef7a7dddec7c242fcf0b470823d01
ThomasBaycroft <thombaycroft@gmail.com>
2025-12-01 17:11:28 +0800

    Add GAIAmodel example

---
+++ src/kima/examples/GaiaBH3/GAIA_free/__init__.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import kima
+from kima import RVData, GAIAdata, RVmodel, BINARIESmodel, GAIAmodel
+from kima.pykima.utils import chdir
+from kima.distributions import Uniform, Gaussian, ModifiedLogUniform, Kumaraswamy, LogUniform, Fixed
+
+__all__ = ['GAIA_free']
+
+here = os.path.dirname(__file__) # cwd
+
+def GAIA_free(run=False, load=False, **kwargs):
+	"""
+	Create (and optionally run) an RV model for analysis of 51 Peg data.
+	This loads Keck/HIRES data from `51Peg.rv` and creates a model where
+	the number of Keplerians is free from 0 to 1.
+
+	Args:
+		run (bool): whether to run the model
+		**kwargs: keyword arguments passed directly to `kima.run`
+	"""
+	# load the right data file
+	data = GAIAdata(os.path.join(here, 'Gaia_astrometry_BH3.gaia'),skip=2,units='mas')
+	data.M0_epoch =  57936.875
+	# create the model
+	model = GAIAmodel(fix=False, npmax=2, data=data)
+	
+	model.Jprior = Fixed(0.5)
+	
+	model.conditional.eprior = Kumaraswamy(0.867,3.03)
+	model.conditional.Pprior = LogUniform(100.0,10000.0)
+	model.conditional.a0prior = ModifiedLogUniform(0.1,100.0)
+	model.conditional.cosiprior = Uniform(-1,1)
+	
+	model.da_prior = Gaussian(4.2,0.4)
+	model.dd_prior = Gaussian(2.4,0.3)
+	model.mua_prior = Gaussian(-0.08,0.2)
+	model.mud_prior = Gaussian(-0.42,0.2)
+	model.parallax_prior = Uniform(1.3,1.9)
+	
+	model.star_mass = 1.2
+	model.RA = 294.82796478104
+	model.DEC = 14.931669719919999
+
+	kwargs.setdefault('steps', 5000)
+	kwargs.setdefault('num_threads', 4)
+	kwargs.setdefault('num_particles', 2)
+	kwargs.setdefault('new_level_interval', 20000)
+	kwargs.setdefault('save_interval', 2000)
+	
+	with chdir(here):
+		if run:
+			kima.run(model, **kwargs)
+		if load:
+			res = kima.load_results(model)
+			return model, res
+
+	return model
+
+if __name__ == '__main__':
+	model = GaiaTest(run=True, steps=80000)
+	res = kima.load_results(model,diagnostic=True)
+
+
--- src/kima/examples/__init__.py
+++ src/kima/examples/__init__.py
@@ -4,5 +4,8 @@ from .BL2009 import BL2009
 from .CoRoT7 import CoRoT7
 from .TIC172900988 import TIC172900988
 from .GaiaBH3.GAIA_free import GAIA_free
+from .GaiaBH3.GAIA import GAIA
+from .GaiaBH3.RVGAIA import RVGAIA
 
-__all__ = ['_51Peg', 'multi_instruments', 'BL2009', 'CoRoT7', 'TIC172900988','GAIA_free']
+
+__all__ = ['_51Peg', 'multi_instruments', 'BL2009', 'CoRoT7', 'TIC172900988','GAIA_free','GAIA','RVGAIA']
