bef13ef162011739cd212f26aae7adc15a4a697b
João Faria <joao.faria@unige.ch>
2026-01-23 12:18:41 +0100

    simpler else

--- src/kima/pykima/results.py
+++ src/kima/pykima/results.py
@@ -3551,9 +3551,8 @@ class KimaResults:
         """
         if self.fix:
             return self.evidence
-        else:
-            from .analysis import compute_values_from_ratios
-            return np.log(compute_values_from_ratios(np.exp(self.evidence), self.ratios))
+        from .analysis import compute_values_from_ratios
+        return np.log(compute_values_from_ratios(np.exp(self.evidence), self.ratios))
 
     def residuals(self, sample, full=False):
         if self.model is MODELS.RVFWHMmodel:
